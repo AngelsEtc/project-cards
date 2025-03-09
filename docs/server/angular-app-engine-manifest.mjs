1d0fe3c3d01d6948d8c31cd8e1408652e9c93b89
AngelsEtc <angelsetcoficial@gmail.com>
2025-03-09 13:31:19 -0300

    first commit

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/project-cards',
+  basePath: 'project-cards',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,7 +2,7 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/project-cards/',
+  baseHref: 'project-cards',
   locale: undefined,
   routes: [
   {
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 510, hash: '2f17fcc0afd4151b77cf9c932eb62f9467151a0feac213913177a1c9188c8367', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1023, hash: '1230655d3cdb83d2070301fec43e43f5caa5645313d9d2346bbe72a196b35966', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 20855, hash: '1119a40f92493bb3d651367afc6eb540fe11bb5d98c0ed0b941997d53ede2ea2', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 17115, hash: 'be1f9a9dac24686f1b68bc1bd72754e69260d946c4e7496ff6db9d29dc100a10', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17152, hash: '4f7899332ea282c6890db86fc7229b1d0d9961ece4eeb0bc28d109342ec5dfff', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 21191, hash: '55134388c92eefff6b2c4d1d25b67f9ea83a26ae30f6e10fc856ff95fdf65ad6', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-HTJ3L7WB.css': {size: 454, hash: 'zg9Y6nw4pg8', text: () => import('./assets-chunks/styles-HTJ3L7WB_css.mjs').then(m => m.default)}
   },
 };
