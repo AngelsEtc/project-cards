796037fc331ed9838134ad03582724b0f10b5ed8
AngelsEtc <angelsetcoficial@gmail.com>
2025-03-11 16:25:04 -0300

    Temporary responsiveness

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 17166, hash: '1c08b7d4cbb7df25c407e8506d2a8f84601ccd3874a293a452b03ce05d3d12fb', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17152, hash: '22998d1010ae3b86ba90089935126c020e2293c076abac63135aeefc6832085c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 23267, hash: 'caf1acca9a424a3941cf11d7441ffd6a67c0113037e53a3eecb306d333758ee4', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 17166, hash: '42533b3d567036c013d1bfb7a672b9902bab452185d7a8dd8b12597973ba88a0', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17152, hash: '8d07a3346555c340b9feea17862ce44d8c2a403ffb9fd4cb6f7889942b04a486', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 22955, hash: 'f8b45a2f02982fb0c21347468bf12b755aeb583cea272aacb39c6ce480ee558e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-J74X3WS3.css': {size: 566, hash: 'Rr8jdXowuEc', text: () => import('./assets-chunks/styles-J74X3WS3_css.mjs').then(m => m.default)}
   },
 };
