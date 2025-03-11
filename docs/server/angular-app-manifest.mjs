5697595fa3094655224c6ba5d16fbf9fd36b397b
AngelsEtc <angelsetcoficial@gmail.com>
2025-03-11 16:35:42 -0300

    Configuration

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 17166, hash: '42533b3d567036c013d1bfb7a672b9902bab452185d7a8dd8b12597973ba88a0', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17152, hash: '8d07a3346555c340b9feea17862ce44d8c2a403ffb9fd4cb6f7889942b04a486', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 22955, hash: 'f8b45a2f02982fb0c21347468bf12b755aeb583cea272aacb39c6ce480ee558e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 17166, hash: 'c2a59068b263734e157becd6f645d27d3960823280687d6f9740c47aa4847bf5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17152, hash: 'ef71ec3cb902b9f1ad1e16e337b14371df2a5aef5df6a8bc96b457e1b3569c24', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 22960, hash: '0da03d5abfe5ee296b9b6a6b0b55744ff3153ce8e45ad398029b5abf304b7ed3', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-J74X3WS3.css': {size: 566, hash: 'Rr8jdXowuEc', text: () => import('./assets-chunks/styles-J74X3WS3_css.mjs').then(m => m.default)}
   },
 };
--- src/app/cardsHome/cardsHome.component.ts
+++ src/app/cardsHome/cardsHome.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cardsHome.component.scss']
 })
 export class CardsHomeComponent implements OnInit {
-imagePathCardChick404: string = '/image/chick404.jpg';
+imagePathCardChick404: string = '/docs/image/chick404.jpg';
 imagePathCardKarobamio: string = '/image/karobamio.jpg';
 imagePathCardOkamivt: string = '/image/okamivt.jpg';
 
