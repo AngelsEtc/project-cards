f684ad7f4b652adb2d60764c948239f9c97ddd10
AngelsEtc <angelsetcoficial@gmail.com>
2025-03-11 22:05:11 -0300

    Update cards

--- docs/browser/server/angular-app-manifest.mjs
+++ docs/browser/server/angular-app-manifest.mjs
@@ -14,7 +14,7 @@ export default {
   assets: {
     'index.csr.html': {size: 17159, hash: '75fe71af816fddb29c93870a8d15f3c45fd6a65cd4f21d4a5df031a7a8f4a0bd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 17154, hash: 'b8e8818e3b810e65ea0fed36848f2a63800d27e450a808b642b164e0f3d52809', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 23521, hash: '0eaaff02a80837121deed6a71d1099e3a170256458469877b852074f56c607f0', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.html': {size: 23520, hash: '16943a32278a1001825c79a9b36bcdc25494dfda26e6cf642177548b8fcdce47', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles.css': {size: 566, hash: 'Rr8jdXowuEc', text: () => import('./assets-chunks/styles_css.mjs').then(m => m.default)}
   },
 };
